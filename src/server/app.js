60e4b49782846edadb233d57b6548f0046d97815
Joaquim d'Souza <joaquimds@gmail.com>
2018-12-12 13:13:13 +0000

    remove static server

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,10 @@
 const createError = require('http-errors')
 const express = require('express')
-const path = require('path')
 const logger = require('morgan')
 
 const app = express()
 
 app.use(logger('dev'))
-app.use(express.static(path.join(__dirname, '..', '..', 'public')))
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
