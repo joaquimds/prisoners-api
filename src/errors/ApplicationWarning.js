600efc3751d444a9ba6598e9d5ab56443f9a8603
Joaquim d'Souza <joaquimds@gmail.com>
2018-12-12 14:44:30 +0000

    improve unique ip error messages

--- src/errors/ApplicationWarning.js
+++ src/errors/ApplicationWarning.js
@@ -4,6 +4,6 @@ class ApplicationWarning extends Error {
 ApplicationWarning.invalid_choice = 'Invalid choice'
 ApplicationWarning.too_late_to_change_choice = 'Too late to change choice'
 ApplicationWarning.too_early_to_choose = 'Too early to choose'
-ApplicationWarning.too_few_unique_ips = 'Waiting for more players with unique IPs...'
+ApplicationWarning.too_few_unique_ips = `Waiting for more players with unique IPs (minimum ${process.env.MINIMUM_UNIQUE_REMOTE_ADDRESSES})...`
 
 module.exports = ApplicationWarning
